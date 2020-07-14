613d0519029557a1b7ea2b1afb8ec6414f0e2f80
wjdxor133 <mahan89@daum.net>
2020-07-14 15:59:26 +0900

    로그인 페이지 완성

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Login from "../src/pages/admin/Login/Login";
+import Activate from "../src/pages/Info/Activate";
+import Info from "../src/pages/Info/Info";
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Switch></Switch>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/activate" component={Activate} />
+          <Route exact path="/info" component={Info} />
+        </Switch>
       </Router>
     );
   }
